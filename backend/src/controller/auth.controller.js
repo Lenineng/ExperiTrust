8f45e4e612a4437bc2af984a9e61b520dd085078
Lenine NGENZI <l.ngenzi@alustudent.com>
2026-02-17 17:03:45 +0200

    Initial commit - ExperiTrust project

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -72,8 +72,8 @@ export const login=async(req, res)=>{
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    generateToken(user._id, res);
-    res.status(200).json(buildUserResponse(user));
+    const token=generateToken(user._id, res);
+    res.status(200).json({...buildUserResponse(user), token});
   } catch (error) {
     console.log("Error in login controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
